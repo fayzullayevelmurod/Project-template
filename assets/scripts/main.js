cb61f0ad8fd261e80c5fd82140c30e7bf497e712
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-12-14 21:06:42 +0500

    folders update

---
+++ assets/scripts/main.js
@@ -0,0 +1,23 @@
+
+let swiper = new Swiper(".treatmentsSwiper", {
+  slidesPerView: 1.10,
+  spaceBetween: 28,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2.2,
+      spaceBetween: 28,
+    },
+    993: {
+      slidesPerView: 3.2,
+      spaceBetween: 28,
+    },
+    1200: {
+      slidesPerView: 3.6,
+      spaceBetween: 40,
+    },
+  },
+});
